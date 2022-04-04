bd2167c46e0589323d1daeeb189b9ecfe60bfd7a
ghamza-dev <elgassaihamzam54@gmail.com>
2022-04-04 22:07:00 +0200

    Allow all headers"

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 header('Access-Control-Allow-Origin: *');
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: 'GET,POST,OPTIONS'");
-header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With,Content-Type, Accept');
+header('Access-Control-Allow-Headers: *');
+// Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With,Content-Type, Accept
 
 define('DS', DIRECTORY_SEPARATOR);
 require_once './config/config.php';
